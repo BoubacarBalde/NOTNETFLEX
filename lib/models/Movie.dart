dee323d93de6a288772b20abadfed036f546c0e8
BoubacarBalde <boubacarbalde859@gmail.com>
2026-04-16 19:18:04 +0100

    Update Movie

--- lib/models/Movie.dart
+++ lib/models/Movie.dart
@@ -1,42 +1,55 @@
 import 'package:notnetflex/services/api.dart';
 
 class Movie {
-
   late final int id;
   late final String name;
   late final String description;
   late final String paterPath;
+  final List<String>? genre;
+  final String? releaseData;
+  final double? vote;
 
-  Movie({required this.id, required this.name, required this.description, required this.paterPath });
-
+  Movie({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.paterPath,
+    this.genre,
+    this.releaseData,
+    this.vote,
+  });
 
   Movie copyWith({
     int? id,
     String? name,
     String? description,
     String? paterPath,
-
-  }){
+    List<String>? genre,
+    String? releaseData,
+    double? vote,
+  }) {
     return Movie(
-        id: id ?? this.id,
-        name: name ?? this.name,
-        description: description ?? this.description,
-        paterPath: paterPath ?? this.paterPath
+      id: id ?? this.id,
+      name: name ?? this.name,
+      description: description ?? this.description,
+      paterPath: paterPath ?? this.paterPath,
+      genre: genre ?? this.genre,
+      releaseData: releaseData ?? this.releaseData,
+      vote: vote ?? this.vote,
     );
   }
 
-  factory Movie.fromJson(Map<String,dynamic> map){
-       return Movie(
-         id: map['id'],
-         name: map['title'] ?? '',
-         description: map['overview'] ?? '',
-         paterPath: map['poster_path'] ?? '',
-       );
+  factory Movie.fromJson(Map<String, dynamic> map) {
+    return Movie(
+      id: map['id'],
+      name: map['title'] ?? '',
+      description: map['overview'] ?? '',
+      paterPath: map['poster_path'] ?? '',
+    );
   }
 
-  String posterURL(){
+  String posterURL() {
     API api = API();
     return api.baseImageURL + paterPath;
   }
-
 }
